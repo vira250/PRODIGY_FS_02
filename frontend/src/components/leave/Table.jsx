6c2c0ba3c002bb46e15347f516203e7d8065a36e
vira250 <jviraj666@gmail.com>
2025-06-17 11:23:00 +0530

    filter the leaves

--- frontend/src/components/leave/Table.jsx
+++ frontend/src/components/leave/Table.jsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import axios from "axios";
 const Table = () =>{
     const [leaves , setLeaves] = useState(null)
+    const [filteredLeaves , setFilteredLeaves] = useState(null)
     const fetchLeaves = async () =>{
         try{
               const response = await axios.get('http://localhost:5000/api/leave',{
@@ -30,6 +31,7 @@ const Table = () =>{
                          action: (<LeaveButtons Id={leave._id}/>)
                       }));
                   setLeaves(data)
+                  setFilteredLeaves(data)
               }
           } catch (error) {
               console.log("Axios error:", error); 
@@ -40,12 +42,35 @@ const Table = () =>{
               }
           }
     }
+
     useEffect(() =>{
         fetchLeaves()
     }, [])
+    const filterByInput = (e) =>{
+        const data = leaves.filter((leave) => 
+            leave.employeeId
+            .toLowerCase()
+            .includes(e.target.value.toLowerCase()) ||
+            leave.name.toLowerCase().includes(e.target.value.toLowerCase()) ||
+            leave.leaveType.toLowerCase().includes(e.target.value.toLowerCase()) ||
+            leave.department.toLowerCase().includes(e.target.value.toLowerCase())
+        )
+        setFilteredLeaves(data)
+    }
+    const filterByButton = (status) =>{
+        const data = leaves.filter((leave) => 
+            leave.status
+            .toLowerCase()
+            .includes(status.toLowerCase()) ||
+            leave.name.toLowerCase().includes(status.toLowerCase()) ||
+            leave.leaveType.toLowerCase().includes(status.toLowerCase()) ||
+            leave.department.toLowerCase().includes(status.toLowerCase())
+        )
+        setFilteredLeaves(data)
+    }
     return(
         <>
-        {leaves ? (
+        {filteredLeaves ? (
         <div className="p-4 bg-white rounded-lg shadow-sm">
             <div className="text-center mb-4">
                 <h3 className="text-xl font-semibold text-gray-800">Manage Leaves</h3>
@@ -55,16 +80,23 @@ const Table = () =>{
       <div className="flex flex-col sm:flex-row items-center justify-between gap-3">
         <input
           type="text"
-          placeholder="Search by Dep Name"
+          placeholder="Search by Emp Id"
           className="px-3 py-1 text-sm border border-gray-300 rounded focus:outline-none focus:ring-1 focus:ring-blue-400 w-full sm:w-1/2"
+          onChange={filterByInput}
           />
         <div className="flex space-x-4">
-        <button className="px-4 py-1.5 text-bold bg-blue-600 text-white rounded hover:bg-blue-700 transition">Pending</button>
-        <button className="px-4 py-1.5 text-bold bg-blue-600 text-white rounded hover:bg-blue-700 transition">Approved</button>
-        <button className="px-4 py-1.5 text-bold bg-blue-600 text-white rounded hover:bg-blue-700 transition">Rejected</button>
+        <button className="px-4 py-1.5 text-bold bg-blue-600 text-white rounded hover:bg-blue-700 transition"
+        onClick={()=> filterByButton("Pending") }>
+            Pending</button>
+        <button className="px-4 py-1.5 text-bold bg-blue-600 text-white rounded hover:bg-blue-700 transition"
+        onClick={()=> filterByButton("Approved") }>
+            Approved</button>
+        <button className="px-4 py-1.5 text-bold bg-blue-600 text-white rounded hover:bg-blue-700 transition"
+        onClick={()=> filterByButton("Rejected") }>
+            Rejected</button>
         </div>
       </div>
-      <DataTable columns={columns} data={leaves}  pagination/>
+      <DataTable columns={columns} data={filteredLeaves}  pagination/>
         </div>
         ) : <div>Loading...</div>}
         </>

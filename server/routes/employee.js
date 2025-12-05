f9c3ee7e88783a18b5ed862e2520328196e8db75
vira250 <jviraj666@gmail.com>
2025-12-05 13:08:24 +0530

    fix error

--- server/routes/employee.js
+++ server/routes/employee.js
@@ -3,11 +3,16 @@ import authMeiddleware from '../middleware/authMiddlware.js'
 import {addEmployee, upload, getEmployees, getEmployee, updateEmployee, fetchEmployeesByDepId} from '../controllers/employeeController.js'
 
 const router = express.Router()
+router.get('/', authMeiddleware, getEmployees);
 
-router.get('/', authMeiddleware, getEmployees)
-router.post('/add', authMeiddleware, upload.single('image'), addEmployee)
-router.get('/:id', authMeiddleware, getEmployee)
-router.put('/:id', authMeiddleware, updateEmployee)
+router.get('/department/:id', authMeiddleware, fetchEmployeesByDepId);
+
+router.post('/add', authMeiddleware, upload.single('image'), addEmployee);
+
+router.get('/:id', authMeiddleware, getEmployee);
+
+router.put('/:id', authMeiddleware, updateEmployee);
+
 
 
 router.get('/department/:id', authMeiddleware, fetchEmployeesByDepId)
